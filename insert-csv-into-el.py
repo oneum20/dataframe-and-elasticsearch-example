9f2640c25014ba3ec3fbb012d4b122489e64d66f
oneum20 <63915349+oneum20@users.noreply.github.com>
2021-06-27 11:18:35 +0900

    Create insert-csv-into-el.py

---
+++ insert-csv-into-el.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
+# Set variables
+csv_file = input("CSV Path : ")
+
+ip = input("ElasticSearch Server IP : ")
+port = input("ElasticSearch Server Port : ")
+
+idx = input("ElasticSearch Index : ")
+type = input("ElasticSearch Type : ")
+
+
+# get csv data
+data = pd.read_csv(csv_file)
+
+
+# config el info
+es = Elasticsearch(host=ip, port=port)
+
+
+# check index & create index
+if not es.indices.exists(index=idx):
+    print("Not exist index... Create index : ", idx)
+    es.indices.create(index=idx,body={})
+ 
+
+ # insert df into el
+documents = data.to_dict(orient='records')
+helpers.bulk(es, documents, index=idx, doc_type=type, raise_on_error=True)
